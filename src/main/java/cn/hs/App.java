6a7febc4bcd96a08c7bfa96a8d54209e790d9845
orangehs <1191066931@qq.com>
2018-01-24 18:28:37 +0800

    Initial commit

---
+++ src/main/java/cn/hs/App.java
@@ -0,0 +1,13 @@
+package cn.hs;
+
+/**
+ * Hello world!
+ *https://github.com/waylau/essential-netty-in-action 测试
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
